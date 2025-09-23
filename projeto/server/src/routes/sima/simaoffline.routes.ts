b6a48a9033a44ccb83b3a2c0bd94238235c4be12
nickdsm24 <nicolas.silva67@fatec.sp.gov.br>
2025-09-22 21:48:56 -0300

    Add getById sima

--- projeto/server/src/routes/sima/simaoffline.routes.ts
+++ projeto/server/src/routes/sima/simaoffline.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAll } from "../../controllers/sima/simaoffline.controller";
+import { getAll, getById } from "../../controllers/sima/simaoffline.controller";
 
 const router = Router();
 
 router.get("/all", getAll);
+router.get("/:idsimaoffline", getById);
 
 export default router;
