505c75c6f76e1d27fc124e463d429f2c29bf0e54
lJesusl-f <pedroenriquejacarei@gmail.com>
2025-09-30 20:12:56 -0300

    Rotas terminadas

--- projeto/server/src/routes/balcar/index.ts
+++ projeto/server/src/routes/balcar/index.ts
@@ -1,8 +1,32 @@
 import express from "express";
 import fluxoinpe from "./fluxoinpe.routes";
+import campanha from "./campanha.routes";
+import instituicao from "./instituicao.routes";
+import sitio from "./sitio.routes";
+import reservatorio from "./reservatorio.routes";
+import tabelacampo from "./tabelacampo.routes";
+
+const app = express();
+
+app.use(express.json());
+
+// Rotas agrupadas por entidade
+app.use("/campanha", campanha);
+app.use("/instituicao", instituicao);
+app.use("/sitio", sitio);
+app.use("/reservatorio", reservatorio);
+app.use("/tabelacampo", tabelacampo);
+
+// Exemplo: GET /campanha?page=1
+
 
 const router = express.Router();
 
 router.use("/fluxoinpe", fluxoinpe);
+router.use("/campanha", campanha);
+router.use("/instituicao", instituicao);
+router.use("/sitio", sitio);
+router.use("/reservatorio", reservatorio);
+router.use("/tabelacampo", tabelacampo);
 
 export default router;
